80486dd50daffd6d3184b454d7e6e7e75afae697
Keenan Lang <klang@aps.anl.gov>
2021-03-12 19:44:10 -0600

    modules.found now contains paths as well

--- libs/modules.lua
+++ libs/modules.lua
@@ -2,19 +2,19 @@
 	Lua library to get informaton from an IOC's envPaths or
 	cdCommands file.
 
-	parseEnvPaths / parseCdCommands - Takes in a path to the
+		parseEnvPaths / parseCdCommands - Takes in a path to the
 		type of file, parses the file to figure out which
 		EPICS modules are included in the IOC. Sets global vars
 		for all the modules with their provided directory paths.
 		Builds package.path and package.cpath (for windows/linux)
 		from each module's lib/<arch>/ directory.
 
-	found - A table of all the defined modules. Only available once
-		the correct parse function is called. Useful for iterating
-		over all modules in an IOC.
+		found - A table of all the defined modules and their paths.
+		Only available once the correct parse function is called.
+		Useful for iterating over all modules in an IOC.
 
-	is_included - Checks if a module name is contained in the 'found'
-		table.
+		is_included - Checks if a module name is contained in the
+		'found' table.
   ]]
 
 local modules = { found = {} }
@@ -35,7 +35,7 @@ function modules.parseEnvPaths(filepath)
 	for line in io.lines(filepath) do
 		local name, value = line:match('epicsEnvSet%("([a-zA-Z0-9_]+)","(.+)"%)')
 
-		table.insert(modules.found, name)
+		modules.found[name] = value
 		epicsEnvSet(name, value)
 
 		if (arch:find("linux")) then
@@ -61,8 +61,8 @@ function modules.parseCdCommands(filepath)
 		local name, value = line:match('putenv%("([a-zA-Z0-9_]+)=(.+)%)')
 
 		if (name ~= nil) then
-			table.insert(modules.found, name)
-			_G[name] = value
+			modules.found[name] = value
+			epicsEnvSet(name, value)
 
 			path = path .. ";" .. value .. separator .. "lib" .. separator .. arch .. separator .. "?.lua"
 		end
@@ -75,12 +75,12 @@ function modules.is_included(module_name)
 	local checkname = module_name:upper()
 
 	for key, value in pairs(modules.found) do
-		if (value == checkname) then
+		if (key == checkname) then
 			return true
 		end
 	end
 
-	return false
+		return false
 end
 
 return modules
